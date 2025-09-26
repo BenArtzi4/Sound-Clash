99f65c0cb12edd4f038795254ef2af4a954d4de7
Ben Artzi Shachar <gal.ben.artzi.shachar@intel.com>
2025-09-26 17:45:28 +0300

    refactor: reorganize frontend components by functionality and purpose

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,4 @@
+// Component exports for cleaner imports
+export { default as Logo } from './common/Logo';
+export { default as ConnectionStatus } from './ui/ConnectionStatus';
+export { default as GenreSelector } from './game/GenreSelector';
